7d77a0570696603154a16bd9fc9c1640763986ef
monika-hrda <mon.hrda@gmail.com>
2022-10-11 13:36:32 +0000

    Add class methods to webhook handler and webhook view
    (set up a listener to listen to messages from Stripe)

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('basket/<hike_id>/', views.view_basket, name='view_basket'),
     path('', views.checkout, name='checkout'),
     path('checkout_success/<booking_id>', views.checkout_success, name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
 ]
