e39440f0132719b89f96f34ed5e65fc702a11041
monika-hrda <mon.hrda@gmail.com>
2022-10-12 15:37:05 +0000

    Expose some fields of user model for updating
    Add user form, view and update profile template

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 from .models import UserProfile
 
 
@@ -11,11 +12,36 @@ class UserProfileForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         """
         Initializes the user profile form - adds placeholders and classes,
-        removes auto-generated labels and sets autofocus on first field
+        removes auto-generated labels
         """
         super().__init__(*args, **kwargs)
 
-        # self.fields['phone_number'].widget.attrs['autofocus'] = True
         self.fields['phone_number'].widget.attrs['placeholder'] = 'Phone Number'
         self.fields['phone_number'].widget.attrs['class'] = 'border-black rounded-1 profile-form-input'
         self.fields['phone_number'].label = False
+
+
+class UserForm(forms.ModelForm):
+    """ Exposes some of the fields of user model for updating """
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialises the user form  - adds placeholders and classes, removes
+        auto-generated labels and sets autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email Address',
+        }
+
+        self.fields['first_name'].widget.attrs['autofocus'] = True
+
+        for field in self.fields:
+            self.fields[field].widget.attrs['placeholder'] = placeholders[field]
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-1 profile-form-input'
+            self.fields[field].label = False
--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
 from .models import UserProfile
-from .forms import UserProfileForm
+from .forms import UserProfileForm, UserForm
 
 
 def profile(request):
@@ -10,19 +10,23 @@ def profile(request):
     user_profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=user_profile)
-        if form.is_valid():
-            form.save()
+        profile_form = UserProfileForm(request.POST, instance=user_profile)
+        user_form = UserForm(request.POST, instance=request.user)
+        if profile_form.is_valid() and user_form.is_valid():
+            profile_form.save()
+            user_form.save()
             messages.success(request, 'Profile updated successfully')
         else:
             messages.error(request, 'Profile update failed. Please make sure your details are valid.')
 
-    form = UserProfileForm(instance=user_profile)
+    profile_form = UserProfileForm(instance=user_profile)
+    user_form = UserForm(instance=user_profile.user)
     bookings = user_profile.bookings.all()
 
     template = 'profiles/profile.html'
     context = {
-        'form': form,
+        'profile_form': profile_form,
+        'user_form': user_form,
         'bookings': bookings,
     }
 
