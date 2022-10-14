9f4f4d03a837ede9ff4ab51bd72ed6c0ed7ae6cf
monika-hrda <mon.hrda@gmail.com>
2022-10-14 00:58:30 +0000

    Add view & url to view details of a past booking

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
+    path('booking_history/<booking_number>', views.booking_history, name='booking_history'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
+from checkout.models import Booking
 from .models import UserProfile
 from .forms import UserProfileForm, UserForm
 
 
 @login_required
 def profile(request):
-    """ Display the user's profile """
+    """ Display and edit the logged in user's profile """
     user_profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -29,6 +31,25 @@ def profile(request):
         'profile_form': profile_form,
         'user_form': user_form,
         'bookings': bookings,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def booking_history(request, booking_number):
+    """ A view to render details of a booking made in the past"""
+    booking = get_object_or_404(Booking, booking_number=booking_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for booking number {booking_number}.'
+        'A confirmation email was sent on the booking date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'booking': booking,
+        'from_profile': True,
     }
 
     return render(request, template, context)
